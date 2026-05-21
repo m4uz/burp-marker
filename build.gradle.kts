c7083e933400c31913903c895eb3f6ad9364cb3e
m4uz <m4uz@proton.me>
2026-05-21 15:56:04 +0200

    chore: burp extension scaffolding

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,16 +3,27 @@ plugins {
 }
 
 group = "m4uz"
-version = "1.0-SNAPSHOT"
+version = "0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("net.portswigger.burp.extensions:montoya-api:2026.4")
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.jar {
+    from({
+        configurations.runtimeClasspath.get().map {
+            if (it.isDirectory) it else zipTree(it)
+        }
+    })
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
 tasks.test {
