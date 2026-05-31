c553a26eb95ef8f42d9e26e32f7ba0b6ddd4cc14
m4uz <m4uz@proton.me>
2026-05-31 16:22:40 +0200

    ci: github release workflow (#8)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,17 @@ plugins {
 }
 
 group = "marker"
-version = "0.0-SNAPSHOT"
+version = "0.0.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+base {
+    archivesName.set("burp-marker")
+}
 
 repositories {
     mavenCentral()
@@ -29,4 +39,11 @@ tasks.jar {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.register("printJarName") {
+    description = "Print the jar archive file name."
+    doLast {
+        println(tasks.jar.get().archiveFileName.get())
+    }
 }
